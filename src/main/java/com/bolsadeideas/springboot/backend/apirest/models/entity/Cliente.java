fe9d891fa6b463b1d3b4197ce671895757e60f54
Alejandro Murillo <alejandro.murillo4@gmail.com>
2023-08-07 18:40:40 -0500

    validacion de datos con anotaciones y valid

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -3,10 +3,17 @@ package com.bolsadeideas.springboot.backend.apirest.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -16,25 +23,29 @@ public class Cliente implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-
-	@NotNull
-	@NotEmpty
-	@Size(min = 4, max = 12)
+	
+	@NotEmpty(message ="no puede estar vacio")
+	@Size(min=4, max=12, message="el tamaño tiene que estar entre 4 y 12")
+	@Column(nullable=false)
 	private String nombre;
-
-	@NotEmpty
+	
+	@NotEmpty(message ="no puede estar vacio")
 	private String apellido;
-
-	@NotNull
-	@NotEmpty
-	@Email
-	@Column(unique = true)
+	
+	@NotEmpty(message ="no puede estar vacio")
+	@Email(message="no es una dirección de correo bien formada")
+	@Column(nullable=false, unique=true)
 	private String email;
 	
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
+	
 	public Long getId() {
 		return id;
 	}
